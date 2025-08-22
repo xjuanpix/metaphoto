4f575b338e5f308b161a4c5cb8b5e3cbee1f8b8d
jp <jp@codebar.dev>
2025-08-22 12:20:56 -0600

    removed lint for now

--- metaphoto-frontend/src/lib/api.ts
+++ metaphoto-frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ import {PhotoModel, PhotoFiltersModel} from "@/types";
 import axios from "axios";
 
 const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://.com'
+  ? 'https://metaphoto-api.codebar.dev'
   : 'http://localhost:3001'
 
 const apiClient = axios.create({
